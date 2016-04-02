ad9a000b32de5b591b9b97fe5e069a5f1f7ed04a
Michal Raška <michal.raska@gmail.com>
2016-04-02 19:14:27 +0200

    Komentáre v runneri, pridané posielanie správy na klienta

--- raspilot_runner/raspilot_runner.py
+++ raspilot_runner/raspilot_runner.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import signal
@@ -9,15 +10,18 @@ from twisted.internet.protocol import Factory, Protocol
 
 from raspilot_implementation.utils.raspilot_logger import RaspilotLoggerFactory
 
+RASPILOT_KILL_TIMEOUT = 5
+
 LOGGER_NAME = 'raspilot_runner'
-
-PIDS_PATH = "../tmp/"
-
-HOST = ''
 PORT = 3002
 
 
-class NewRaspilotRunner:
+class RaspilotRunner:
+    """
+    Spawns new Raspilot instance upon receiving request to do so. Only one Raspilot instance can be running at the time.
+    When the Raspilot stops, it should notify the RaspilotRunner, which kills the Raspilot instance if does not exit
+    within given time.
+    """
     def __init__(self):
         self.__logger = logging.getLogger(LOGGER_NAME)
         self.__logger.info("Starting Raspilot Runner")
@@ -28,15 +32,20 @@ class NewRaspilotRunner:
         raspilot_endpoint.listen(RaspilotProtocolFactory(RaspilotProtocol(self)))
 
         spawn_endpoint = TCP4ServerEndpoint(reactor, PORT)
-        spawn_endpoint.listen(RaspilotProtocolFactory(RaspilotSpawnProtocol(self)))
+        self.__protocol = RaspilotSpawnProtocol(self)
+        spawn_endpoint.listen(RaspilotProtocolFactory(self.__protocol))
 
         self.__raspilot_process = None
 
     def on_raspilot_message(self):
+        """
+        Called after receiving message from Raspilot instance. Waits for the specified time and then kills the Raspilot.
+        :return: returns nothing
+        """
         try:
             if self.__raspilot_process:
                 self.__logger.info('Waiting for Raspilot to exit...')
-                self.__raspilot_process.wait(5)
+                self.__raspilot_process.wait(RASPILOT_KILL_TIMEOUT)
         except subprocess.TimeoutExpired:
             self.__logger.error('Killing Raspilot it because did not exit')
             self.__raspilot_process.kill()
@@ -44,6 +53,11 @@ class NewRaspilotRunner:
             self.__raspilot_process = None
 
     def on_spawn_request(self):
+        """
+        Called after receiving the spawn request. If no Raspilot instance is running, spawns the Raspilot and notifies
+        the requesting client otherwise only send message to the client, nothing is spawned.
+        :return: returns nothing
+        """
         if not self.__raspilot_process:
             self.__logger.info('Spawning new Raspilot')
             my_dir = os.path.dirname(__file__)
@@ -53,13 +67,27 @@ class NewRaspilotRunner:
             self.__raspilot_process = subprocess.Popen(['python3', path_to_script], stdout=subprocess.DEVNULL,
                                                        stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
             self.__logger.info("Raspilot running with PID {}".format(self.__raspilot_process.pid))
+            if self.__protocol.transport:
+                message = self.__create_spawn_message('Raspilot ready')
+                self.__protocol.transport.write(self.__encode_message(message))
         else:
+            if self.__protocol.transport:
+                message = self.__create_spawn_message('Raspilot already running')
+                self.__protocol.transport.write(self.__encode_message(message))
             self.__logger.warning('Raspilot already running')
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Called upon exiting the 'with block'. If Raspilot instance was left running, sends SIGINT, and waits for
+        Raspilot to exit. If Raspilot does not exit within given time, kills it.
+        @param exc_type:
+        @param exc_val:
+        @param exc_tb:
+        @return: returns nothing
+        """
         if self.__raspilot_process:
             try:
                 self.__logger.info("Asking Raspilot with PID {} to exit".format(self.__raspilot_process.pid))
@@ -70,8 +98,36 @@ class NewRaspilotRunner:
                 self.__logger.error("Raspilot is still running, killing it")
                 self.__raspilot_process.kill()
 
+    @staticmethod
+    def __encode_message(message):
+        """
+        Encodes given message as JSON. Returns bytes array representing the JSON when encoded with utf-8.
+        :param message: message to be encoded
+        :return: bytes array representing the JSON when encoded with utf-8
+        :rtype: dict
+        """
+        json_data = json.dumps(message)
+        json_data += '\n'
+        return bytes(json_data.encode('utf-8'))
+
+    def __create_spawn_message(self, message, spawned=True, error=None):
+        """
+        Creates spawn message which is sent to the client.
+        :param message: message which should be human readable
+        :param spawned: boolean flag, if spawn was successful
+        :param error: explanation of error, if an error occurred
+        :return: message which is sent to the client
+        :rtype: dict
+        """
+        return {'message': message, 'spawned': spawned, 'error': error,
+                'myAddress': self.__protocol.transport.client[0]}
+
 
 class RaspilotProtocol(Protocol):
+    """
+    Simple protocol, which calls the callback method on_raspilot_message upon receiving data. Used when checking
+    whether the Raspilot exits or not.
+    """
     def __init__(self, callbacks):
         self.__callbacks = callbacks
 
@@ -80,6 +136,9 @@ class RaspilotProtocol(Protocol):
 
 
 class RaspilotSpawnProtocol(Protocol):
+    """
+    Simple protocol, which calls the callback method on_spawn_request upon receiving data. Used when spawning Raspilot.
+    """
     def __init__(self, callbacks):
         self.__callbacks = callbacks
 
@@ -98,7 +157,7 @@ class RaspilotProtocolFactory(Factory):
 if __name__ == "__main__":
     my_dir = os.path.dirname(__file__)
     logs_path = os.path.join(my_dir, '../logs/')
-    RaspilotLoggerFactory.create(LOGGER_NAME, logging.DEBUG, os.path.abspath(logs_path))
-    runner = NewRaspilotRunner()
+    RaspilotLoggerFactory.create(LOGGER_NAME, logging.DEBUG, logs_path)
+    runner = RaspilotRunner()
     with runner:
         reactor.run()

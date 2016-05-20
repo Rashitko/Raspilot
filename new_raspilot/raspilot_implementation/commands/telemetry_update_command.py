8e4942961676e36add1e59d944a0dc01422e5bac
Michal Raška <michal.raska@gmail.com>
2016-05-20 20:27:15 +0200

    Pridaná utilizácia CPU do telemetrie

--- new_raspilot/raspilot_implementation/commands/telemetry_update_command.py
+++ new_raspilot/raspilot_implementation/commands/telemetry_update_command.py
@@ -13,6 +13,6 @@ class TelemetryUpdateCommand(BaseCommand):
     @classmethod
     def create_from_system_state(cls, system_state):
         data = {'orientation': system_state.get('orientation', None), 'location': system_state.get('location', None),
-                'batteryLevel': system_state.get('batteryLevel', None)}
+                'flightControllerStatus': system_state.get('flightControllerStatus', None)}
         c = TelemetryUpdateCommand(data)
         return c

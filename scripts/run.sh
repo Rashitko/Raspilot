ec041a252673c28237ab420ffae100602699e962
Michal Raška <michal.raska@gmail.com>
2016-12-04 13:47:24 +0100

    Scripty

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+source ~/.bashrc
+source ~/.bash_profile
+
 set -e
 
-RASPILOT_ROOT=/home/raspilot/raspilot-app
+BRANCH=raspilot-production
+CONFIG_PATH=new_raspilot/config/config.cfg
 
-PYTHONPATH=${RASPILOT_ROOT} python3.5 ${RASPILOT_ROOT}/new_raspilot/raspilot_implementation/main.py
+if grep -iq "autoupdate\s*=\s*enabled" ${RASPILOT_PATH}/${CONFIG_PATH}; then
+echo "Loading new version"
+    git fetch --all > /dev/null
+    git checkout ${BRANCH} > /dev/null
+    git reset --hard origin/${BRANCH} > /dev/null
+else
+    echo "Autoupdate Disabled"
+fi
+
+echo "Starting Raspilot"
+PYTHONPATH=${RASPILOT_PATH} python3.5 ${RASPILOT_PATH}/new_raspilot/raspilot_implementation/main.py

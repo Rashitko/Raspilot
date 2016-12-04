8ac47bed1fc9881501a8a221f4cdcdde0ff096da
Michal Raška <michal.raska@gmail.com>
2016-12-04 16:29:00 +0100

    Scripty

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-source ~/.bashrc
-source ~/.bash_profile
-
 set -e
 
 BRANCH=raspilot-production
 CONFIG_PATH=new_raspilot/config/config.cfg
 
 if grep -iq "autoupdate\s*=\s*enabled" ${RASPILOT_PATH}/${CONFIG_PATH}; then
+CURRENT_DIR=$(pwd)
 echo "Loading new version"
+    cd ${RASPILOT_PATH}
     git fetch --all > /dev/null
     git checkout ${BRANCH} > /dev/null
     git reset --hard origin/${BRANCH} > /dev/null
+    cd ${CURRENT_DIR}
 else
     echo "Autoupdate Disabled"
 fi

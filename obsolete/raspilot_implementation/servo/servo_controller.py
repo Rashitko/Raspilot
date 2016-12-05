db4b9ee299f09152a377ca5ef9eaeaebe6e49831
Michal Raška <michal.raska@gmail.com>
2016-12-05 08:34:35 +0100

    Odstranene obsolete veci

--- obsolete/raspilot_implementation/servo/servo_controller.py
+++
@@ -1,9 +0,0 @@
-from obsolete.raspilot.servo import ServoController, ServoControllerConfig
-
-
-class RaspilotServoController(ServoController):
-    pass
-
-
-class RaspilotServoControllerConfig(ServoControllerConfig):
-    pass
